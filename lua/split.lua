fafc77963babe54707d51daa8b9f384160bececa
Arman.H <ahhdev@gmail.com>
2022-05-23 21:20:57 +0430

    add stylua + format

--- lua/split.lua
+++ lua/split.lua
@@ -1,15 +1,15 @@
 local function is_match_empty(pat, plain)
-  return not not string.find('', pat, nil, plain)
+  return not not string.find("", pat, nil, plain)
 end
 
 --- String split into table
 function string:split(sep, plain)
   local str = self
   local b, res = 0, {}
-  sep = sep or '%s+'
+  sep = sep or "%s+"
 
-  assert(type(sep) == 'string')
-  assert(type(str) == 'string')
+  assert(type(sep) == "string")
+  assert(type(str) == "string")
 
   if #sep == 0 then
     for i = 1, #str do
@@ -18,14 +18,16 @@ function string:split(sep, plain)
     return res
   end
 
-  assert(not is_match_empty(sep, plain), 'delimiter can not match empty string')
+  assert(not is_match_empty(sep, plain), "delimiter can not match empty string")
 
   while b <= #str do
     local e, e2 = string.find(str, sep, b, plain)
     if e then
-      res[#res + 1] = string.sub(str, b, e-1)
+      res[#res + 1] = string.sub(str, b, e - 1)
       b = e2 + 1
-      if b > #str then res[#res + 1] = "" end
+      if b > #str then
+        res[#res + 1] = ""
+      end
     else
       res[#res + 1] = string.sub(str, b)
       break
