20fc2ae9c03def39b0090f1cae6f28fce2d7a002
jeff <1105041+jeff-dh@users.noreply.github.com>
2022-12-04 15:56:27 +0100

    fixed the fix

--- lua/jabs.lua
+++ lua/jabs.lua
@@ -78,7 +78,7 @@ function M.setup(c)
     }
 
     -- Use devicons file symbols
-    M.use_devicons = c.use_devicons or true
+    M.use_devicons = not (c.use_devicons == false)
 
     -- Fallback file symbol for devicon
     M.default_file = c.symbols.default_file or ""
